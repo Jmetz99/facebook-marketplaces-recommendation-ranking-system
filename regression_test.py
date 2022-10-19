bc5b7da962917e1f371cc80277349962c00a3b4f
Jmetz99 <jacobmetz12@gmail.com>
2022-10-19 16:00:33 +0100

    Adds code for prediction of price using linear regression

--- regression_test.py
+++ regression_test.py
@@ -1,13 +1,17 @@
+from re import X
 import numpy as np
 import pandas as pd
-from sklearn import model_selection
+from sklearn import linear_model
 from sklearn.feature_extraction.text import TfidfVectorizer
-import itertools
+from sklearn import metrics
+from matplotlib import pyplot as plt
+from scipy import sparse
 
 df = pd.read_csv('cleaned_products.csv', lineterminator="\n")
 
 prices = df['price']
 
+# Drop unused features
 df = df.drop('price', axis=1)
 df = df.drop('Unnamed: 0.1', axis=1)
 df = df.drop('Unnamed: 0', axis=1)
@@ -21,35 +25,26 @@ test_data = df.drop(training_features.index)
 training_outputs = prices.sample(frac=0.7, random_state=0)
 test_outputs = prices.drop(training_outputs.index)
 
-product_names = training_features['product_name'].tolist()
-product_description = training_features['product_description'].tolist()
-product_location = training_features['location'].tolist()
+# Isolate features
+product_names = training_features['product_name']
+product_description = training_features['product_description']
+product_location = training_features['location']
 
+product_test = training_features
+
+# Get TF-IDF scores
 vectorizer = TfidfVectorizer()
+description_response = vectorizer.fit_transform(product_description).toarray()
+names_response = vectorizer.fit_transform(product_names).toarray()
+location_response = vectorizer.fit_transform(product_location).toarray()
 
-names_response = vectorizer.fit_transform(product_names)
+responses_concat = np.concatenate((names_response, description_response, location_response), axis=1)
+sparse_responses = sparse.csr_matrix(responses_concat)
 
-print(names_response)
+# Regress and predict 
+model = linear_model.LinearRegression()
+model.fit(sparse_responses, training_outputs)
+y_pred = model.predict(sparse_responses)
 
-
-# description_response = vectorizer.fit_transform(product_description)
-# for col in description_response.nonzero()[1]:
-#     print (feature_names[col], ' - ', description_response[0, col])
-
-# location_response = vectorizer.fit_transform(product_location)
-# for col in location_response.nonzero()[1]:
-#     print (feature_names[col], ' - ', location_response[0, col])
-
-
-class LinearRegression:
-    def __init__(self, n_features: int): # initalise parameters
-        np.random.seed(10)
-        self.W = np.random.randn(n_features, 1) ## randomly initialise weight
-        self.b = np.random.randn(1) ## randomly initialise bias
-        
-    def __call__(self, X):
-        ypred = np.dot(X, self.W) + self.b
-        return ypred # return prediction
-    
-    def update_params(self, W, b):
-        self.W = W 
+# Evaluate MSE of linear model(=8.3131305359371)
+print(metrics.mean_squared_error(training_outputs, y_pred))
