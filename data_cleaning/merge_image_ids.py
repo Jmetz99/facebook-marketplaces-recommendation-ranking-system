e6d0258a2aa1f07c3853b65fdc4018bcc1a1e35b
Jmetz99 <jacobmetz12@gmail.com>
2022-10-23 14:20:14 +0100

    retitles merge_ids

--- data_cleaning/merge_image_ids.py
+++ data_cleaning/merge_image_ids.py
@@ -3,17 +3,17 @@ from PIL import Image
 from numpy import asarray
 import os
 
-cleaned_products_df = pd.read_csv('cleaned_products.csv', lineterminator="\n")
+cleaned_products_df = pd.read_csv('/Users/jacobmetz/Documents/GitHub/facebook-marketplaces-recommendation-ranking-system/data/cleaned_products.csv', lineterminator="\n")
 cleaned_products_df = cleaned_products_df.drop('Unnamed: 0.1', axis=1)
 cleaned_products_df = cleaned_products_df.drop('Unnamed: 0', axis=1)
 cleaned_products_df = cleaned_products_df.rename({'id': 'product_id'}, axis=1)
 
-df = pd.read_csv('Images.csv')
+df = pd.read_csv('/Users/jacobmetz/Documents/GitHub/facebook-marketplaces-recommendation-ranking-system/data/Images.csv')
 
 merged_df = pd.merge(cleaned_products_df, df, on='product_id')
 merged_df = merged_df.drop('Unnamed: 0', axis=1)
 
-file_names = os.listdir('/Users/jacobmetz/Documents/GitHub/facebook-marketplaces-recommendation-ranking-system/resized_images') 
+file_names = os.listdir('/Users/jacobmetz/Documents/GitHub/facebook-marketplaces-recommendation-ranking-system/data/resized_images') 
 
 ids = []
 arrays = []
@@ -21,7 +21,7 @@ arrays = []
 for file in file_names:
     id = file.strip('.jpg_resized.jpg')
     ids.append(id)
-    img = Image.open(f'resized_images/{file}')
+    img = Image.open(f'/Users/jacobmetz/Documents/GitHub/facebook-marketplaces-recommendation-ranking-system/data/resized_images/{file}')
     image_array = asarray(img)
     arrays.append(image_array)
 
@@ -32,4 +32,4 @@ image_df = pd.concat([ids_series, arrays_series], axis=1)
 
 full_df = pd.merge(merged_df, image_df, on='id')
 
-full_df.to_pickle('/Users/jacobmetz/Documents/GitHub/facebook-marketplaces-recommendation-ranking-system/full_df.pickle')
+full_df.to_pickle('/Users/jacobmetz/Documents/GitHub/facebook-marketplaces-recommendation-ranking-system/data/full_df.pickle')
